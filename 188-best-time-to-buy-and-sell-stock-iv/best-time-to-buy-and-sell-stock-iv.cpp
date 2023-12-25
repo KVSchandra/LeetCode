17e8cd0a2e53c6064a9e06b9dc4bf23ee7c3c936
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2023-12-25 12:32:28 +0530

    Time: 26 ms (23.87%) | Memory: 14.9 MB (21.32%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int solve(int i, int buy, int transaction, int &n, vector<int>&prices, vector<vector<vector<int>>> &dp){
+    int solve(int i, int buy, int k, int &n, vector<int>&prices, vector<vector<vector<int>>>&dp){
         if(i == n) return 0;
-        if(transaction == 0) return 0;
-        if(dp[i][buy][transaction] != -1) return dp[i][buy][transaction];
-        if(buy == 1) {
-            int buys = -prices[i] + solve(i+1, 0, transaction, n, prices, dp);
-            int notBuys = 0 + solve(i+1, 1, transaction, n, prices, dp);
-            return dp[i][buy][transaction] = max(buys, notBuys);
+        if(k == 0) return 0;
+        if(dp[i][buy][k] != -1) return dp[i][buy][k];
+        if(buy) {
+            int buys = -prices[i] + solve(i+1, 0, k, n, prices, dp);
+            int notBuys = 0 + solve(i+1, 1, k, n, prices, dp);
+            return dp[i][buy][k] = max(buys, notBuys);
         }
         else {
-            int sell = prices[i] + solve(i+1, 1, transaction-1, n, prices, dp);
-            int notSell = 0 + solve(i+1, 0, transaction, n, prices, dp);
-            return dp[i][buy][transaction] = max(sell, notSell);
+            int sell = prices[i] + solve(i+1, 1, k-1, n, prices, dp);
+            int notSell = 0 + solve(i+1, 0, k, n, prices, dp);
+            return dp[i][buy][k] = max(sell, notSell);
         }
     }
 
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
         return solve(0, 1, k, n, prices, dp);
     }
 };

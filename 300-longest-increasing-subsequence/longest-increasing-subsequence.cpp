5aafb4faaa31da19bdaef073de675b5aa599cedd
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2023-12-25 17:49:58 +0530

    Time: 947 ms (6.04%) | Memory: 292.6 MB (11.39%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int solve(int ind, int prevInd, int &n, vector<int>&nums, vector<vector<int>>&dp) {
+    int solve(int ind, int prevInd, int &n, vector<int>&nums, vector<vector<int>>&dp){
         if(ind == n) return 0;
-        if(dp[ind][prevInd+1] != -1) return dp[ind][prevInd+1];
+        if(dp[ind][prevInd + 1] != -1) return dp[ind][prevInd + 1];
         int take = INT_MIN;
-        if(prevInd == -1 || nums[prevInd] < nums[ind]) {
-            take = 1 + solve(ind+1, ind, n, nums, dp);
+        if(prevInd == -1 || nums[prevInd] < nums[ind]){
+            take = 1 + solve(ind + 1, ind, n, nums, dp);
         }
-        int notTake = 0 + solve(ind+1, prevInd, n, nums, dp);
-        return dp[ind][prevInd+1] = max(take, notTake);
+        int notTake = 0 + solve(ind + 1, prevInd, n, nums, dp);
+        return dp[ind][prevInd + 1] = max(take, notTake);
     }
 
     int lengthOfLIS(vector<int>& nums) {

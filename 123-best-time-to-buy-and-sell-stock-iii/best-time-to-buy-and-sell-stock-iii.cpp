8df9a20288ebebdcaa7dc8f84894fc469bde8435
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2023-12-26 00:22:15 +0530

    Time: 585 ms (13.88%) | Memory: 218.3 MB (12.83%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int solve(int i, int buy, int transaction, int &n, vector<int>&prices, vector<vector<vector<int>>>&dp){
-        if(transaction == 0) return 0;
+    int solve(int i, int buy, int k, int &n, vector<int>&prices, vector<vector<vector<int>>>&dp) {
         if(i == n) return 0;
-        if(dp[i][buy][transaction] != -1) return dp[i][buy][transaction];
-        if(buy == 1){
-            int buys = -prices[i] + solve(i+1, 0, transaction, n, prices, dp);
-            int notBuys = 0 + solve(i+1, 1, transaction, n, prices, dp);
-            return dp[i][buy][transaction] = max(buys, notBuys);
+        if(k == 0) return 0;
+        if(dp[i][buy][k] != -1) return dp[i][buy][k];
+        if(buy) {
+            int buys = -prices[i] + solve(i+1, 0, k, n, prices, dp);
+            int notBuys = 0 + solve(i+1, 1, k, n, prices, dp);
+            return dp[i][buy][k] = max(buys, notBuys);
         }
-        else{
-            int sell = prices[i] + solve(i+1, 1, transaction-1, n, prices, dp);
-            int notSell = 0 + solve(i+1, 0, transaction, n, prices, dp);
-            return dp[i][buy][transaction] = max(sell, notSell);
+        else {
+            int sell = prices[i] + solve(i+1, 1, k-1, n, prices, dp);
+            int notSell = 0 + solve(i+1, 0, k, n, prices, dp);
+            return dp[i][buy][k] = max(sell, notSell);
         }
+ 
     }
 
     int maxProfit(vector<int>& prices) {

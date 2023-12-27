78a6e25f562fa333da5e345b91b9c8f899024d02
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2023-12-28 02:00:24 +0530

    Time: 9 ms (5.06%) | Memory: 7.3 MB (6.16%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int solve(int i, int sum, int prod, vector<int>&v){
+    int solve(int i, int sum, vector<int>&v, vector<vector<int>>&dp){
         if(i == v.size()) return 0;
-        if(sum == 0) return prod;
+        if(sum == 0) return 1;
+        if(dp[i][sum] != -1) return dp[i][sum];
         int pick = INT_MIN;
-        if(sum - v[i] >= 0) pick = v[i] * solve(i, sum-v[i], prod, v);
-        int notPick = solve(i+1, sum, prod, v);
-        return max(pick, notPick);
+        if(sum - v[i] >= 0) pick = v[i] * solve(i, sum-v[i], v, dp);
+        int notPick = solve(i+1, sum, v, dp);
+        return dp[i][sum] = max(pick, notPick);
     }
 
     int integerBreak(int n) {
         vector<int>v;
         for(int i = 1;i<n;i++) v.push_back(i);
         int sum = n;
-        int prod = 1;
-        return solve(0, sum, prod, v);
+        vector<vector<int>>dp(n, vector<int>(59, -1));
+        return solve(0, sum, v, dp);
     }
 };

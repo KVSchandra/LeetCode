eb801d7cb6d535b68b807f80ee6e1f5cfeea0add
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2023-12-30 02:51:51 +0530

    Time: 740 ms (5.24%) | Memory: 505.9 MB (5.24%) - LeetSync

---
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+     int solve(vector<int>& jobs, int ind, int d, int maxi, vector<vector<vector<int>>>&dp) {
+         if (d == 1) return *max_element(jobs.begin() + ind, jobs.end());
+         if(ind > jobs.size()-d) return INT_MAX;
+         if(dp[ind][d][maxi] != -1) return dp[ind][d][maxi];
+         maxi = max(maxi, jobs[ind]);
+         int pick = maxi + solve(jobs, ind + 1, d - 1, 0, dp);
+         int not_pick = solve(jobs, ind + 1, d, maxi, dp);
+         return dp[ind][d][maxi] = min(pick, not_pick);
+     }
+
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        if (jobDifficulty.size() < d) return -1;
+        int n = jobDifficulty.size();
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(d+1, vector<int>(10001, -1)));
+        return solve(jobDifficulty, 0, d, 0, dp);
+    }
+};

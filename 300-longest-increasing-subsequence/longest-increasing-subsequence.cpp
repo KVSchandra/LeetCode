72f3e4b4a064bdff7aaa50112446ee6a1b723e42
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-01-05 21:53:42 +0530

    Time: 869 ms (12.63%) | Memory: 292.7 MB (11.08%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int solve(int i, int prevInd, int &n, vector<int>&nums, vector<vector<int>>&dp) {
+    int solve(int i, int prevInd, int n, vector<int>&nums, vector<vector<int>>&dp){
         if(i == n) return 0;
-        if(dp[i][prevInd+1] != -1) return dp[i][prevInd+1];
-        int take = 0;
-        if(prevInd == -1 || nums[i] > nums[prevInd]) {
-            take = 1 + solve(i+1, i, n, nums, dp);
+        int pick = INT_MIN;
+        if(dp[i][prevInd + 1] != -1) return dp[i][prevInd + 1];
+        if(prevInd == -1 || nums[prevInd] < nums[i]){
+            pick = 1 + solve(i+1, i, n, nums, dp);
         }
-        int notTake = 0 + solve(i+1, prevInd, n, nums, dp);
-        return dp[i][prevInd + 1] = max(take, notTake);
+        int notPick = 0 + solve(i+1, prevInd, n, nums, dp);
+        return dp[i][prevInd + 1] = max(pick, notPick);
     }
 
     int lengthOfLIS(vector<int>& nums) {

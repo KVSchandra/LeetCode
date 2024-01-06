5865f1a21cbdaf2714c9d0166d5b203454e2e1ce
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-01-06 16:02:15 +0530

    Time: 3 ms (51.32%) | Memory: 8 MB (5.08%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int solve(int k, vector<int>&nums, vector<int>&dp) {
+    int solve(int i, int k, vector<int>&nums, vector<vector<int>>&dp) {
+        if(i == nums.size()) return 0;
         if(k == 0) return 1;
-        if(dp[k] != -1) return dp[k];
-        int count = 0;
-        for(int i = 0; i<nums.size(); i++){
-            if(k - nums[i] >= 0) count += solve(k - nums[i], nums, dp);
+        if(dp[i][k] != -1) return dp[i][k];
+        int pick = 0;
+        if(k - nums[i] >= 0){
+            pick = solve(0, k-nums[i], nums, dp);
         }
-        return dp[k] = count;
+        int notPick = solve(i+1, k, nums, dp);
+        return dp[i][k] = pick + notPick;
     }
 
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target + 1, -1);
-        return solve(target, nums, dp);
+        int n = nums.size();
+        vector<vector<int>>dp(n, vector<int>(target+1, -1));
+        return solve(0, target, nums, dp);
     }
 };

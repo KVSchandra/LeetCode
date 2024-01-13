f596c569bf9c843bf054d10370a12b3d84c7d85e
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-01-13 23:12:24 +0530

    Time: 14 ms (41.13%) | Memory: 10.6 MB (64.33%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -3,44 +3,44 @@ public:
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
-        vector<vector<int>>vis(n, vector<int>(m, 0));
         queue<pair<int, int>>q;
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(i == 0 || i == n-1 || j == 0 || j == m-1){
                     if(board[i][j] == 'O'){
-                        vis[i][j] = 1;
                         q.push({i, j});
                         board[i][j] = '#';
                     }
                 }
             }
         }
-        vector<int>v = {0, 1, 0, -1, 0};
+        vector<int> dir = {0, 1, 0, -1 ,0};
         while(!q.empty()){
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            for(int i = 0; i < 4; i++){
-                int Dx = x + v[i];
-                int Dy = y + v[i+1];
-                if(Dx>=0 && Dy>=0 && Dx<n && Dy<m && !vis[Dx][Dy] && board[Dx][Dy]=='O'){
-                    vis[Dx][Dy] = 1;
-                    board[Dx][Dy] = '#';
-                    q.push({Dx, Dy});
-                }
-            }
-        }
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(board[i][j] == '#' && vis[i][j] == 1){
-                    board[i][j] = 'O';
-                }
-                else if(board[i][j] == 'O' && vis[i][j] == 0){
-                    board[i][j] = 'X';
+            int size = q.size();
+            while(size--) {
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                for(int i = 0; i<4; i++){
+                    int dx = x + dir[i];
+                    int dy = y + dir[i+1];
+                    if(dx>=0 && dx<n && dy>=0 && dy<m && board[dx][dy] == 'O'){
+                        board[dx][dy] = '#';
+                        q.push({dx, dy});
+                    }
                 }
             }
         }
 
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m ;j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }
+                else if(board[i][j] == '#'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
     }
 };

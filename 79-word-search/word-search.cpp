3813ea7f7ccffd616141bb113836fe48a3c1f764
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-01-16 00:20:54 +0530

    Time: 2135 ms (5.02%) | Memory: 378.3 MB (5.11%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -5,17 +5,17 @@ public:
         int m = board[0].size();
         if(ind == word.size()-1) return true;
         vis[i][j] = 1;
-        pathVis[i][j] = 1;
+        // pathVis[i][j] = 1;
         vector<int>dir = {0, 1, 0, -1, 0};
         for(int k = 0; k<4; k++){
             int dx = i + dir[k];
             int dy = j + dir[k+1];
-            if(dx>=0 && dx<n && dy>=0 && dy<m && !vis[dx][dy] && board[dx][dy] == word[ind+1]){
+            if(dx>=0 && dx<n && dy>=0 && dy<m && !vis[dx][dy] && board[dx][dy] == word[ind+1]  ){
                 if(solve(ind+1, dx, dy, word, board, vis, pathVis)) return true;
             }
         }
         vis[i][j] = 0;
-        pathVis[i][j] = 0;
+        // pathVis[i][j] = 1;
         return false;
     }
 

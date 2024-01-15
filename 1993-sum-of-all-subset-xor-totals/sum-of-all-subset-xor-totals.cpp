206aed25e477176d8c1fb25b8539c13606d55878
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-01-16 01:16:38 +0530

    Time: 3 ms (71.65%) | Memory: 8 MB (26.15%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int solve(int i, int x, int n, vector<int>&nums){
+    int solve(int i, int x, int n, vector<int>&nums, vector<vector<int>>&dp){
         if(i == n) return x;
-        int pick = solve(i+1, x^nums[i], n, nums);
-        int notPick = solve(i+1, x, n, nums);
-        return pick + notPick;
+        if(dp[i][x] != -1) return dp[i][x];
+        int pick = solve(i+1, x^nums[i], n, nums, dp);
+        int notPick = solve(i+1, x, n, nums, dp);
+        return dp[i][x] = pick + notPick;
     }
 
     int subsetXORSum(vector<int>& nums) {
-        int x = 0;
         int n = nums.size();
-        return solve(0, x, n, nums);
+        int x = 0;
+        // int sum = 0;
+        vector<vector<int>>dp(n, vector<int>(100, -1));
+        return solve(0, x, n, nums, dp);
     }
 };

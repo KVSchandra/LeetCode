c7c9c1a68e5baa2b64dd5ecba0b121fd1c65f125
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-01-19 13:27:46 +0530

    Time: 15 ms (39.09%) | Memory: 11 MB (61.52%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int solve(int i,int j,int &n,vector<vector<int>>&matrix,vector<vector<int>>&dp){
-        if(i > n-1 || j > n-1 || j < 0) return 1000000;
+    int solve(int i, int j, int n, vector<vector<int>>&matrix, vector<vector<int>>&dp){
+        if(i >= n || j >= n || j < 0) return 100001;
         if(i == n-1) return matrix[i][j];
         if(dp[i][j] != 101) return dp[i][j];
-        int down = matrix[i][j] + solve(i+1,j,n,matrix,dp);
-        int diag_left = matrix[i][j] + solve(i+1,j-1,n,matrix,dp);
-        int diag_right = matrix[i][j] + solve(i+1,j+1,n,matrix,dp);
-        return dp[i][j] = min(down,min(diag_left,diag_right));
+        int down = matrix[i][j] + solve(i+1, j, n, matrix, dp);
+        int diag_left = matrix[i][j] + solve(i+1, j-1, n, matrix, dp);
+        int diag_right = matrix[i][j] + solve(i+1, j+1, n, matrix, dp);
+        return dp[i][j] = min(down, min(diag_left, diag_right));
     }
 
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>>dp(n,vector<int>(n,101));
         int mini = INT_MAX;
-        for(int j = 0;j<n;j++){
-            mini = min(solve(0,j,n,matrix,dp),mini);
+        vector<vector<int>>dp(n, vector<int>(n, 101));
+        for(int j = 0; j<n; j++){
+            mini = min(mini, solve(0, j, n, matrix, dp));
         }
         return mini;
     }

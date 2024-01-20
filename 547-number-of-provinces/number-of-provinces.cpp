040e2b13e4b814b4223c180504bf2e5584def462
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-01-21 02:00:15 +0530

    Time: 21 ms (72.88%) | Memory: 17.9 MB (8.63%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,32 +1,53 @@
-class Solution {
-private: 
-    void dfs(int node, vector<int>&vis, vector<int>adj[]){
-        vis[node] = 1;
-        for(auto val : adj[node]){
-            if(!vis[val]){
-                dfs(val, vis, adj);
-            }
+class DisjointSet{
+    public:
+    vector<int>rank, parent;
+    DisjointSet(int n){
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        for(int i = 0; i<=n; i++){
+            parent[i] = i;
         }
     }
+
+    int findUParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUParent(u); 
+        int ulp_v = findUParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+};
+
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int>adj[n];
+        int m = isConnected[0].size();
+        DisjointSet ds(n);
+        int count = 0;
         for(int i = 0; i<n; i++){
-            for(int j =0 ; j<n; j++){
+            for(int j = 0; j<m; j++){
                 if(isConnected[i][j]){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                    ds.unionByRank(i, j);
                 }
             }
         }
-        vector<int>vis(n, 0);
-        int count = 0;
         for(int i = 0; i<n; i++){
-            if(!vis[i]){
-                dfs(i, vis, adj);
-                count++;
-            }
+            if(ds.parent[i] == i) count++;
         }
         return count;
     }

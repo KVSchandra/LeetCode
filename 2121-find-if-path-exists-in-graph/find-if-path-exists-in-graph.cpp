4010cd8f6e1e2018c1580a567ef403af566d0893
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-01-22 12:44:38 +0530

    Time: 299 ms (99.15%) | Memory: 137 MB (90.18%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,23 +1,64 @@
-class Solution {
-public:
-    void dfs(int node, vector<int>&vis, vector<int>adj[]){
-        vis[node] = 1;
-        for(auto val : adj[node]){
-            if(!vis[val]){
-                dfs(val, vis, adj);
-            }
+class DisjointSet{
+    public:
+    vector<int>rank, parent, size;
+    DisjointSet(int n){
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
         }
     }
 
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<int>vis(n, 0);
-        vector<int>adj[n];
-        for(auto val : edges){
-            adj[val[0]].push_back(val[1]);
-            adj[val[1]].push_back(val[0]);
+    int findUParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUParent(u); 
+        int ulp_v = findUParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
         }
-        dfs(source, vis, adj);
-        if(vis[destination]) return true;
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUParent(u); 
+        int ulp_v = findUParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+};
+
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        DisjointSet ds(n);
+        if(n == 1) return true;
+        for(auto val : edges){
+            if(ds.findUParent(val[0]) != ds.findUParent(val[1])){
+                ds.unionBySize(val[0], val[1]);
+            }
+            if(ds.findUParent(source) == ds.findUParent(destination)) return true;
+        }
         return false;
     }
 };

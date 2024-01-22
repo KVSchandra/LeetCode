c780effbc73c864e98be31b519674984b0e32fd8
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-01-23 04:36:02 +0530

    Time: 145 ms (86.36%) | Memory: 77 MB (5.30%) - LeetSync

---
+++ 1223-graph-connectivity-with-threshold/graph-connectivity-with-threshold.cpp
@@ -0,0 +1,69 @@
+class DisjointSet{
+    public:
+    vector<int>rank, parent, size;
+    DisjointSet(int n){
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUParent(u); 
+        int ulp_v = findUParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUParent(u); 
+        int ulp_v = findUParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+};
+
+
+class Solution {
+public:
+    vector<bool> areConnected(int n, int threshold, vector<vector<int>>& queries) {
+        DisjointSet ds(n);
+        for(int i = 0; i<=n; i++){
+            for(int j = 1; j<=sqrt(i); j++){
+                if(i%j == 0){ 
+                    if(j > threshold) ds.unionByRank(i, j);
+                    if((i/j) > threshold) ds.unionByRank(i, i/j);
+                }
+            }
+        }
+        vector<bool>ans;
+        for(auto val : queries){
+            ans.push_back(ds.findUParent(val[0]) == ds.findUParent(val[1]));
+        }
+        return ans;
+    }
+};

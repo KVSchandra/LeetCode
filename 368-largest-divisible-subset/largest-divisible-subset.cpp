cc0c043d04624f02b3f0de83d8d6c9ca368ba21e
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-01-28 12:10:27 +0530

    Time: 28 ms (85.50%) | Memory: 11.4 MB (13.16%) - LeetSync

---
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void solve(int i, int prevInd, int &n, vector<int>&nums, vector<int>&ans, vector<int>&temp, vector<int>&dp) {
+        if(i >= n) {
+            if(temp.size() > ans.size()) ans = temp;
+            return;
+        }
+        // int pick = INT_MIN;
+        if((prevInd == -1 || nums[i] % nums[prevInd] == 0) && ((int)temp.size() > dp[i])) {
+            dp[i] = temp.size();
+            temp.push_back(nums[i]);
+            solve(i+1, i, n, nums, ans, temp, dp);
+            temp.pop_back();
+        }
+        solve(i+1, prevInd, n, nums, ans, temp, dp);
+        // return max(pick, notPick);
+    }
+
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int> ans;
+        vector<int>temp;
+        vector<int>dp(n+1, -1);
+        solve(0, -1, n, nums, ans, temp, dp);
+        // cout<<a<<endl;
+        return ans;
+    }
+};

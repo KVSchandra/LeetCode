a7090df58d99723d7797f17285703c3f83dcbfb7
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-02-05 00:31:00 +0530

    Time: 232 ms (35.69%) | Memory: 96.2 MB (20.97%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    static bool comp(string &s1, string &s2){
-        if(s1.size() > s2.size()) return true;
-        else if(s1.size() < s2.size()) return false;
-        else {
-            for(int i = 0; i<s1.size(); i++){
-                if(s1[i] > s2[i]) return true;
-                else if(s1[i] < s2[i]) return false;
-            }
-            return false;
-        }
-    }
-
     string kthLargestNumber(vector<string>& nums, int k) {
-        sort(nums.begin(), nums.end(), comp);
-        int n = nums.size();
-        for(auto val : nums) cout<<val<<" ";
-        return nums[k-1];
+        priority_queue<pair<int, string>>pq;
+        for(auto val : nums){
+            pq.push({val.size(), val});
+        }
+        k--;
+        while(k--){
+            pq.pop();
+        }
+        return pq.top().second;
     }
 };

40f8f9e1ac8b9ec8334a47235ccfd88d52b1b61b
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-02-15 02:37:56 +0530

    Time: 82 ms (97.28%) | Memory: 70.2 MB (57.19%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        // \U0001f602\U0001f602\U0001f602\U0001f602
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};

eafbce0120affa23428118f494c01885ee72c704
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-03-13 19:05:47 +0530

    Time: 92 ms (41.65%) | Memory: 80 MB (47.38%) - LeetSync

---
+++ 2664-maximize-greatness-of-an-array/maximize-greatness-of-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximizeGreatness(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int i = 0, j = 0, count = 0;
+        while(j < nums.size()){
+            if(nums[j] > nums[i]){
+                i++; 
+                count++; 
+            }
+            j++;
+        }
+        return count;
+    }
+};

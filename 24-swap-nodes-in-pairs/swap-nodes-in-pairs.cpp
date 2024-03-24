f3e0b7c3039458e0407adec3cb094a756a30b4d6
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-03-24 12:10:31 +0530

    Time: 4 ms (38.24%) | Memory: 9.4 MB (83.21%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -11,11 +11,20 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
         ListNode*temp = head;
-        while(temp && temp->next){
-            swap(temp->val, temp->next->val);
-            temp = temp->next->next;
+        ListNode*temp1 = head->next;
+        ListNode*head1 = temp1;
+        ListNode*forward = NULL;
+        while(temp1 && temp) {
+            forward = temp1->next;
+            temp1->next = temp;
+            if(forward && forward->next) temp->next = forward->next;
+            else temp->next = forward;
+            temp = forward;
+            if(temp) temp1 = temp->next;
+            else temp1 = NULL;
         }
-        return head;
+        return head1;
     }
 };

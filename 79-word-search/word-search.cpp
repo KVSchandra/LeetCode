c7e16c60f422fab6d45c9ee05ba666edbd478364
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-04-03 11:37:27 +0530

    Time: 1525 ms (5.80%) | Memory: 380.3 MB (5.00%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool solve(int ind, int i, int j,string &word, vector<vector<char>> &board, vector<vector<int>>&vis, vector<vector<int>>&pathVis){
+    bool solve(int ind, int i, int j,string &word, vector<vector<char>> &board, vector<vector<int>>&vis){
         int n = board.size();
         int m = board[0].size();
         if(ind == word.size()-1) return true;
@@ -10,7 +10,7 @@ public:
             int dx = i + dir[k];
             int dy = j + dir[k+1];
             if(dx>=0 && dx<n && dy>=0 && dy<m && !vis[dx][dy] && board[dx][dy] == word[ind+1]){
-                if(solve(ind+1, dx, dy, word, board, vis, pathVis)) return true;
+                if(solve(ind+1, dx, dy, word, board, vis)) return true;
             }
         }
         vis[i][j] = 0;
@@ -21,11 +21,10 @@ public:
         int n = board.size();
         int m = board[0].size();
         vector<vector<int>>vis(n, vector<int>(m, 0));
-        vector<vector<int>>pathVis(n, vector<int>(m, 0));
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(board[i][j] == word[0]){
-                    if(solve(0, i, j, word, board, vis, pathVis)) return true;
+                    if(solve(0, i, j, word, board, vis)) return true;
                 }
             }
         }

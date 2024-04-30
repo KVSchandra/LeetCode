9e58d9a626daa5aae7734e8b510b1656205f5b31
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-04-30 06:06:57 +0530

    Time: 428 ms (23.35%) | Memory: 20.9 MB (7.19%) - LeetSync

---
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        long long count = 0;
+        unordered_map<int, int>mp;
+        mp[0] = 1;
+        long long mask = 0;
+        for(long long i = 0; i<word.size(); i++) {
+            mask ^= (1 << (word[i]-'a'));
+            count += mp[mask];
+            for(long long j = 0; j<10; j++) {
+                mask ^= 1 << j;
+                count += mp[mask];
+                mask ^= 1 << j;
+            }
+            mp[mask]++;
+        }
+        for(auto val : mp) {
+            bitset<10>bit(val.first);
+            cout<<bit<<" "<<val.second<<endl;
+        }
+        return count;
+    }
+};

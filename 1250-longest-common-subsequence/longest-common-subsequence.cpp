9bb879587a08fbb6d4081a5714f0357da53ff6cc
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-05-02 14:07:17 +0530

    Time: 15 ms (92.08%) | Memory: 25.6 MB (50.94%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -12,12 +12,21 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size(), m = text2.size();
         vector<vector<int>>dp(n+1, vector<int>(m+1));
-        for(int i = n-1; i >= 0; i--) {
-            for(int j = m-1; j >= 0; j--) {
-                if(text1[i] == text2[j]) dp[i][j] = 1 + dp[i+1][j+1];
-                else dp[i][j] = 0 + max(dp[i+1][j], dp[i][j+1]);
+        for(int i = 1; i<=n; i++) {
+            for(int j = 1; j<=m; j++) {
+                if(text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = 0 + max(dp[i-1][j], dp[i][j-1]);
             }           
         }
-        return dp[0][0];
+        return dp[n][m];
+        // vector<int> curr(m+1, 0), prev(m+1, 0);
+        // for(int i = n-1; i>=0; i--) {
+        //     for(int j = m-1; j>=0; j--) {
+        //         if(text1[i] == text1[j]) curr[j] = 1 + prev[j+1];
+        //         else curr[j] = max(prev[j], curr[j+1]);
+        //     }
+        //     prev = curr;
+        // }
+        // return curr[0];
     }
 };

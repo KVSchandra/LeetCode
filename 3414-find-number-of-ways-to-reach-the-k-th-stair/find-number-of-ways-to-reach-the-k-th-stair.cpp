edfdfeb8a33e271fd38bc03fff6b389c295ea685
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-05-20 15:52:04 +0530

    Time: 251 ms (20.00%) | Memory: 120.1 MB (20.00%) - LeetSync

--- 3414-find-number-of-ways-to-reach-the-k-th-stair/find-number-of-ways-to-reach-the-k-th-stair.cpp
+++ 3414-find-number-of-ways-to-reach-the-k-th-stair/find-number-of-ways-to-reach-the-k-th-stair.cpp
@@ -5,7 +5,7 @@ public:
         if(i > k + 1) return 0; 
         if(dp[i][jump].find(prev) != dp[i][jump].end()) return dp[i][jump][prev];
         ll pick1 = 0;
-        if (prev == 0 && i - 1 >= 0) pick1 = solve(i - 1, 1, jump, k, dp);
+        if(prev == 0 && i - 1 >= 0) pick1 = solve(i - 1, 1, jump, k, dp);
         ll pick2 = solve(i + (1 << jump), 0, jump + 1, k, dp);
         ll ans = pick1 + pick2;
         if(i == k) ans++;

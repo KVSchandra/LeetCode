640660227194a43292b2a22366da9ca90d8f2bf4
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-05-23 22:23:40 +0530

    Time: 70 ms (31.49%) | Memory: 44.7 MB (19.06%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        unordered_map<int, int>mp;
+        for(int i = 0; i<profit.size(); i++) {
+            if(mp.count(difficulty[i])) {
+                if(mp[difficulty[i]] < profit[i]) {
+                    mp[difficulty[i]] = profit[i];
+                }
+            }
+            else {
+                mp[difficulty[i]] = profit[i];
+            }
+            
+        }
+        sort(difficulty.begin(), difficulty.end());
+        vector<int>prefix(difficulty.size(), 0);
+        int maxi = INT_MIN;
+        for(int i = 0; i<difficulty.size(); i++) {
+            maxi = max(maxi, mp[difficulty[i]]);
+            prefix[i] = maxi;
+        }
+        int sum = 0;
+        // for(int i = 0; i<difficulty.size(); i++) {
+        //     cout << '{' << difficulty[i] << ',' << prefix[i] << '}' << " ";
+        // }
+        for(int i = 0; i<worker.size(); i++) {
+            auto it = upper_bound(difficulty.begin(), difficulty.end(), worker[i]);
+            if(it != difficulty.end()) {
+                int idx = it - difficulty.begin();
+                idx--;
+                if(idx < 0) {
+                    continue;
+                }
+                sum += prefix[idx];
+            }
+            else {
+                sum += prefix.back();
+            }
+        }
+        return sum;
+    }
+};

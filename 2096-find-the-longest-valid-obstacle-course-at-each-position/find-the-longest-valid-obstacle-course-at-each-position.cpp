f6cfe55c014c73afab9075d6422847963c3fcb81
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-05-24 21:54:19 +0530

    Time: 660 ms (5.25%) | Memory: 202.6 MB (5.25%) - LeetSync

--- 2096-find-the-longest-valid-obstacle-course-at-each-position/find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 2096-find-the-longest-valid-obstacle-course-at-each-position/find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // SEGMENT TREES :- vector<int> seg(1000), vector<int>arr(1000), i = 0, l = 0, h = n-1, x = idx, v = val, (x,y) = (l,r).
     void buildSeg(vector<int>& seg, const vector<int>& arr, int i, int l, int h) {
         if (l == h) {
             seg[i] = arr[l];
@@ -10,28 +11,23 @@ public:
         buildSeg(seg, arr, 2*i + 2, m + 1, h);
         seg[i] = max(seg[2*i + 1], seg[2*i + 2]);
     }
-
     void updateSeg(vector<int>& seg, vector<int>& arr, int i, int l, int h, int x, int v) {
-        if(l == h) {
+        if (l == h) {
             arr[x] = v;
             seg[i] = v;
             return;
         }
         int m = (l + h) / 2;
-        if(x <= m)
-            updateSeg(seg, arr, 2*i + 1, l, m, x, v);
-        else
-            updateSeg(seg, arr, 2*i + 2, m + 1, h, x, v);
+        (x <= m) ? updateSeg(seg, arr, 2*i + 1, l, m, x, v) : updateSeg(seg, arr, 2*i + 2, m + 1, h, x, v);
         seg[i] = max(seg[2*i + 1], seg[2*i + 2]);
     }
-
     int querySeg(const vector<int>& seg, int i, int l, int h, int x, int y) {
         if (l > y || h < x) return INT_MIN;
         if (l >= x && h <= y) return seg[i];
         int m = (l + h) / 2;
-        int leftMax = querySeg(seg, 2*i + 1, l, m, x, y);
-        int rightMax = querySeg(seg, 2*i + 2, m + 1, h, x, y);
-        return max(leftMax, rightMax);
+        int leftMin = querySeg(seg, 2*i + 1, l, m, x, y);
+        int rightMin = querySeg(seg, 2*i + 2, m + 1, h, x, y);
+        return max(leftMin, rightMin);
     }
 
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {

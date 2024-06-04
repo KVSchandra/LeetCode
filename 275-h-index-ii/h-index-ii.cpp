f6e2f6e36e8d6be9eb7194104514e62b1819d44a
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-06-04 17:58:36 +0530

    Time: 18 ms (13.70%) | Memory: 20.8 MB (91.87%) - LeetSync

--- 275-h-index-ii/h-index-ii.cpp
+++ 275-h-index-ii/h-index-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int maxi = INT_MIN;
-        sort(citations.begin(), citations.end());
+        // sort(citations.begin(), citations.end());
         int n = citations.size();
         for(int i = 0; i<n; i++) {
             maxi = max(maxi, min(citations[i], n-i));

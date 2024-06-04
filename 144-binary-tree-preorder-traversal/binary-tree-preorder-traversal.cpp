30f422b00b1caa65ee7ba348477dcb1e3624680d
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-06-04 22:37:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (48.06%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -10,18 +10,36 @@
  * };
  */
 class Solution {
-void preorder(TreeNode*root,vector<int>&ans){
-    if(root == NULL) return;
-
-    ans.push_back(root->val);
-    preorder(root->left,ans);
-    preorder(root->right,ans);
-}
-
 public:
+    // MORRIS TRAVERSAL
+    // Threaded Binary Tree
+    // TC : Amortised(N) + O(N) => O(N) 
+    // SC : O(1)
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        preorder(root,ans);
-        return ans;
+        vector<int>preorder;
+        if(root == NULL) return preorder;
+        TreeNode* curr = root;
+        while(curr != NULL) {
+            if(curr->left == NULL) {
+                preorder.push_back(curr->val);
+                curr = curr->right;
+            }
+            else {
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right != curr) {
+                    prev = prev->right;
+                }
+                if(prev->right == NULL) {
+                    prev->right = curr;
+                    preorder.push_back(curr->val);
+                    curr = curr->left;
+                }
+                else {
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+            }
+        } 
+        return preorder;
     }
 };

862e14f42d70c92de57f1fcfe1fee0953f9d3ded
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-06-05 17:22:16 +0530

    Time: 10 ms (54.05%) | Memory: 16.8 MB (6.16%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        queue<pair<int, int>>q;
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                if(matrix[i][j] == 0) {
+                    q.push({i, j});
+                }
+            }
+        }
+        while(!q.empty()) {
+            auto val = q.front();
+            q.pop();
+            for(int i = 0; i<n; i++) {
+                matrix[i][val.second] = 0;
+            }
+            for(int j = 0; j<m; j++) {
+                matrix[val.first][j] = 0;
+            }
+        }
+    }
+};

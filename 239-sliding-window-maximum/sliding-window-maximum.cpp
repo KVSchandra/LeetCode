3c188d9b99c008e55fe85f8bba36f67951db1210
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-07-25 00:22:20 +0530

    Time: 173 ms (56.90%) | Memory: 137.3 MB (39.77%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) 
-    {
-        int n=arr.size();
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int>ans;
-        map<int,int>mp;
-        int maxi=0;
-        set<int>st;
-        for(int i=0;i<k;i++)
-        {
-            st.insert(arr[i]);
-            mp[arr[i]]++;
-        }
-        /*for(auto it:st)
-        {
-            cout<<it<<endl;
-        }*/
-        //cout<<*(st.rbegin())<<endl;
-        ans.push_back(*(st.rbegin()));
-        int j=k;
-        int i=0;
-        while(j<n){
-            mp[arr[j]]++;
-            st.insert(arr[j]);
-            mp[arr[i]]--;
-            if(mp[arr[i]]==0)
-            {
-               mp.erase(arr[i]);
-               st.erase(arr[i]);
-               ans.push_back(*(st.rbegin()));
+        deque<int>dq;
+        int i = 0;
+        int j = 0;
+        while(j < nums.size()){
+            while(dq.size() && dq.back() < nums[j]){
+                dq.pop_back();
             }
-            else
-            {
-                ans.push_back(*(st.rbegin()));
+            dq.push_back(nums[j]);
+            if(j-i+1 < k){
+                j++;
             }
-           i++;j++;
+            else if(j-i+1 == k){
+                ans.push_back(dq.front());
+                if(dq.front() == nums[i]){
+                    dq.pop_front();
+                }
+                i++;
+                j++;
+            }
         }
         return ans;
     }

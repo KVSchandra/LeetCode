259c7a10ed0bd532545bfa770a0ddf16368af914
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-07-31 21:13:19 +0530

    Time: 9 ms (49.49%) | Memory: 10.4 MB (23.52%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -10,19 +10,22 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         if(head == NULL || head->next == NULL) return NULL;
+
         ListNode*slow = head;
         ListNode*fast = head;
-        while(fast && fast->next){
+        ListNode*entry = head;
+
+        while(fast->next && fast->next->next){
             slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast) break;
+            if(slow == fast){
+                while(slow != entry){
+                    slow = slow->next;
+                    entry = entry->next;
+                }
+                return entry;
+            }
         }
-        if(fast == NULL || fast->next == NULL) return NULL;
-        fast = head;
-        while(fast != slow){
-            slow = slow->next;
-            fast = fast->next;
-        }
-        return fast;
+        return NULL;
     }
 };

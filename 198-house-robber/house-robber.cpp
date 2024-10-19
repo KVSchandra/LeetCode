e19bd3c9375cba11b0d21a11a7e56b05e13f2785
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-10-20 01:01:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (5.31%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,15 @@
+#define ll long long
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>dp(n, 0);
-        dp[0] = nums[0];
-        for(int i = 1; i<n; i++){
-            int pick = nums[i];
-            if(i > 1) pick = nums[i] + dp[i-2];
-            int notPick = 0 + dp[i-1];
-            dp[i] = max(pick, notPick);
+        ll n = nums.size();
+        vector<ll> dp(n+2, INT_MIN);
+        dp[n] = 0;
+        dp[n+1] = 0;
+        for(ll i = n-1; i >= 0; i--) {
+            dp[i] = max(dp[i], dp[i+1]);
+            dp[i] = max(dp[i], nums[i] + dp[i+2]);
         }
-        return dp[n-1];
+        return dp[0];
     }
 };

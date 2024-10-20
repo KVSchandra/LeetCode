b3191dae27e58574e921eb17ae6e9d1a04bfbc8c
K Venkat Sai Chandra <146515779+KVSchandra@users.noreply.github.com>
2024-10-20 21:19:48 +0530

    Time: 17 ms (28.35%) | Memory: 20.7 MB (8.91%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,16 +1,19 @@
+#define ll long long
 class Solution {
 public:
-
     int minDistance(string word1, string word2) {
-        int n1 = word1.size();
-        int n2 = word2.size();
-        vector<vector<int>>dp(n1+1, vector<int>(n2+1, 0));
-        for(int i = n1-1; i>=0; i--) {
-            for(int j = n2-1; j>=0; j--) {
-                if(word1[i] == word2[j]) dp[i][j] = 1 + dp[i+1][j+1];
-                else dp[i][j] = 0 + max(dp[i][j+1], dp[i+1][j]);
+        ll n = word1.size(), m = word2.size();
+        vector<vector<ll>> dp(n+1, vector<ll>(m+1));
+        for(ll i = n-1; i >= 0; i--) {
+            for(ll j = m-1; j >= 0; j--) {
+                if(word1[i] == word2[j]) {
+                    dp[i][j] = max(dp[i][j], 1 + dp[i+1][j+1]);
+                }
+                else {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                }
             }
         }
-        return n1 + n2 - 2*dp[0][0];
+        return (n + m - 2*dp[0][0]);
     }
 };
